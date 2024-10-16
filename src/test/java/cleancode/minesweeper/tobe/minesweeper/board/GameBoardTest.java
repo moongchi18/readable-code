ddc56c838198f670fad3b47ba5b327154e673f25
moongchi18 <lsmunt2@gmail.com>
2024-10-17 08:32:10 +0900

    feature: 게임 난이도별 gameBoard 초기화 테스트 생성

--- src/test/java/cleancode/minesweeper/tobe/minesweeper/board/GameBoardTest.java
+++ src/test/java/cleancode/minesweeper/tobe/minesweeper/board/GameBoardTest.java
@@ -1,19 +1,24 @@
 package cleancode.minesweeper.tobe.minesweeper.board;
 
+import cleancode.minesweeper.tobe.minesweeper.board.position.CellPosition;
 import cleancode.minesweeper.tobe.minesweeper.gamelevel.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
+import static java.util.stream.Stream.of;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class GameBoardTest {
     @ParameterizedTest
     @MethodSource("ofGameLevels")
-    @DisplayName("게임판 초기화 테스트")
+    @DisplayName("게임을 시작하면 게임레벨에 맞춰 게임보드를 초기화한다.")
     void initializeGame(GameLevel gameLevel) {
         // given
         GameBoard gameBoard = new GameBoard(gameLevel);
@@ -35,12 +40,54 @@ class GameBoardTest {
 //        @TODO gameBoard의 board에서 numberCell 업데이트 확인
     }
 
+    @ParameterizedTest
+    @MethodSource("ofGameLevels")
+    @DisplayName("선택된 cellPosition의 row와 col 유효성을 검사한다.")
+    void isInvalidCellPosition(GameLevel gameLevel) {
+        // given
+        GameBoard gameBoard = new GameBoard(gameLevel);
+        gameBoard.initializeGame();
+
+        int levelRowSize = gameLevel.getRowSize();
+        int levelColSize = gameLevel.getColSize();
+
+        CellPosition cellPosition = CellPosition.of(levelRowSize - 1, levelColSize - 1);
+        CellPosition invalidRowPosition = CellPosition.of(levelRowSize - 1, levelColSize);
+        CellPosition invalidColPosition = CellPosition.of(levelRowSize, levelColSize - 1);
+        CellPosition invalidPosition = CellPosition.of(levelRowSize, levelColSize);
+
+        // when
+        boolean isValidPosition = gameBoard.isInvalidCellPosition(cellPosition);
+        boolean isInvalidRowPosition = gameBoard.isInvalidCellPosition(invalidRowPosition);
+        boolean isInvalidColPosition = gameBoard.isInvalidCellPosition(invalidColPosition);
+        boolean isInvalidPosition = gameBoard.isInvalidCellPosition(invalidPosition);
+
+        // then
+        System.out.println(gameBoard.getRowSize() + " " + gameBoard.getColSize());
+        System.out.println(cellPosition.getRowIndex() + ", " + cellPosition.getColIndex());
+        assertThat(isValidPosition).isFalse();
+        assertThat(isInvalidRowPosition).isTrue();
+        assertThat(isInvalidColPosition).isTrue();
+        assertThat(isInvalidPosition).isTrue();
+
+        assertThat(CellPosition.of(0,0)).isNotNull();
+        assertThatThrownBy(() -> CellPosition.of(-1, 0))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> CellPosition.of(0, -1))
+            .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> CellPosition.of(-1, -1))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
+//  ==================== private =======================
     private static Stream<Arguments> ofGameLevels(){
-        return Stream.of(
-            Arguments.arguments(new Advanced()),
-            Arguments.arguments(new Middle()),
-            Arguments.arguments(new Beginner()),
-            Arguments.arguments(new VeryBeginner())
+        return of(
+            arguments(new Advanced()),
+            arguments(new Middle()),
+            arguments(new Beginner()),
+            arguments(new VeryBeginner())
         );
     }
 }
